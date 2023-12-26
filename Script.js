28fdc3accd427078e9d2bb25002ed3b70ef8e73e
Sourav Sekhar Behera <121654372+Sourav143-rgb@users.noreply.github.com>
2023-12-26 11:15:54 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,61 @@
+code -->
+  /*============toogle icon navbar======*/
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuIcon.onclick= () => {
+   menuIcon.classList.toogle('bx-x');
+   navbar.classList.toggle('active');
+
+};
+
+
+
+
+
+/*============scroll sections active link======*/
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+
+window.onscroll= () => {
+    sections.forEach(sec =>{
+let top=window.scrollY;
+let offset=sec.offsetTop - 150;
+let height=sec.offsetHeight;
+let id=sec.getAttribute('id');
+
+     if(top >= offset && top < offset + height){
+        navlinks.forEach(links => {
+         links.classList.remove('active');
+         document.querySelector('header nav a[href*=' +id+ ']').classList.add('active');
+        });
+     };
+  });
+
+  let header=document.querySelector('header');
+
+  header.classList.toggle('sticky',window.scrollY > 100);
+
+  /*============remove toggle icon and navbar when click navbar link(scroll)======*/
+  menuIcon.classList.remove('bx-x');
+   navbar.classList.remove('active');
+};
+
+ScrollReveal({ //reset: true,
+    distance:'80px',duration:2000,delay:200
+
+});
+
+ScrollReveal().reveal('.Home-content, .heading', {origin: 'top' });
+ScrollReveal().reveal('.Home-img, .Services-container,.Portfolio-box,.Contact form ',{origin: 'bottom' });
+ScrollReveal().reveal('.Home-content h1,.About-img ',{origin: 'left' });
+ScrollReveal().reveal('.Home-content p,.About-content ',{origin: 'right' });
+
+/*=====Typed js=====*/
+const typed=new Typed('.multiple-text',{
+
+    strings:['Frontend Developer','UX Designer','Student'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
